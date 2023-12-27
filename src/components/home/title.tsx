2fc20d6676f7615d93a60d93db0de7944fefd9f6
ManoloRaj <manolo.rajaonah@gmail.com>
2023-12-27 22:45:37 +0100

    Minimum content

---
+++ src/components/home/title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+    return (
+        <div className="font-bold text-7xl">
+            Ambohitantara <br/>
+            History Blog
+        </div>
+    )
+}
--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -1,7 +1,9 @@
+import Title from "./home/title";
+
 export default function Presentation() {
     return (
-        <div className="border border-black w-full h-screen">
-            Salut les gars
+        <div className="w-full h-screen bg-[url('../assets/background.png')] bg-cover">
+            <Title/>
         </div>
 
     )
